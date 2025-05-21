1e80907543b39a521643c9d4687ef25025a1c270
carlosrjhoe <carlosrjhoe@gmail.com>
2025-05-21 20:40:29 -0300

    FEAT: Projeto iniciado

--- books/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from books.models import Books
-
-class BooksSerializers(ModelSerializer):
-    class Meta:
-        model = Books
-        fields = '__all__'
--- books/api/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from books.api import serializers
-from books import models
-
-class BooksViewsets(viewsets.ModelViewSet):
-    queryset = models.Books.objects.all()
-    serializer_class  = serializers.BooksSerializers
