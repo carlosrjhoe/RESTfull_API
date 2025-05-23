b2c89c5c5bbdce7ac2143d810a36968d9f8e8821
carlosrjhoe <carlosrjhoe@gmail.com>
2025-05-23 20:23:01 -0300

    REFACTOR: Ajustando automação da API

--- util/register_books.py
+++ util/register_books.py
@@ -2,6 +2,7 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from faker import Faker
 from time import sleep
 import random
@@ -13,6 +14,7 @@ class ApiRegisterBook:
 
     def __init__(self):
         self.driver = Chrome()
+        self.wait = WebDriverWait(self.driver, 5)
         self.faker = Faker('pt_BR')
         self.title = (By.NAME, 'title')
         self.author = (By.NAME, 'author')
@@ -28,35 +30,42 @@ class ApiRegisterBook:
             "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima",
             "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"
         ]
-        self.button_post = (By.CSS_SELECTOR, "button.btn.btn-primary.js-tooltip")
 
     def open(self):
         self.driver.get(self.URL)
         self.driver.maximize_window()
         sleep(1)
 
-    def register_books(self):
-        title = self.faker.sentence(nb_words=4)
-        author = self.faker.name()
-        release_year = random.randint(1900, 2025)
-        state = random.choice(self.estados_brasil)
-        pages = random.randint(50, 1000)
-        publishing_company = self.faker.company()
-        self.driver.find_element(*self.title).send_keys(title)
-        self.driver.find_element(*self.author).send_keys(author)
-        self.driver.find_element(*self.release_year).send_keys(release_year)
-        self.driver.find_element(*self.state).send_keys(state)
-        self.driver.find_element(*self.pages).send_keys(pages)
-        self.driver.find_element(*self.publishing_company).send_keys(publishing_company)
-
     def send_register(self):
-        buttons = self.driver.find_elements(*self.button_post)
-        button_post = buttons[0]
-        button_post.click()
+        self.wait.until(EC.presence_of_all_elements_located(By.CSS_SELECTOR, "button.btn.btn-primary.js-tooltip"))
+        buttons = self.driver.find_elements(By.CSS_SELECTOR, "button.btn.btn-primary.js-tooltip")
+        if len(buttons) > 2:
+            buttons[2].click()
+            ActionChains(self.driver).move_to_element(By.CSS_SELECTOR, "button.btn.btn-primary.js-tooltip").perform()
+            self.wait.until(EC.element_to_be_clickable(By.CSS_SELECTOR, "button.btn.btn-primary.js-tooltip"))
+            button_post.click()
 
+    def register_books(self):
+        for _ in range(5):
+            title = self.faker.sentence(nb_words=4)
+            author = self.faker.name()
+            release_year = random.randint(1900, 2025)
+            state = random.choice(self.estados_brasil)
+            pages = random.randint(50, 1000)
+            publishing_company = self.faker.company()
+            self.driver.find_element(*self.title).send_keys(title)
+            self.driver.find_element(*self.author).send_keys(author)
+            self.driver.find_element(*self.release_year).send_keys(release_year)
+            self.driver.find_element(*self.state).send_keys(state)
+            self.driver.find_element(*self.pages).send_keys(pages)
+            self.driver.find_element(*self.publishing_company).send_keys(publishing_company)
+            self.send_register()
+
+    def close(self):
+        self.driver.quit()
 
 if __name__ == '__main__':
     registar_livro = ApiRegisterBook()
     registar_livro.open()
     registar_livro.register_books()
-    registar_livro.send_register()
+    registar_livro.close()
